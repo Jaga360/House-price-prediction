eeeb8ad618e17f0d19bda02e52f0f504176f3bdc
Jaga360 <kingsmanjagadeesh@gmail.com>
2021-04-09 21:18:44 +0530

    change architecture to single server and build predict api

--- server/app.py
+++ server/app.py
@@ -9,23 +9,28 @@ __location=None
 
 @app.route('/getLocation')
 def getLocation():
+
     with open('artifacts/house_prediction_columns.json' , ) as f:
         location =  json.load(f)
-        global __location
-        __location = location["data_columns"][3:]
-    return json.dumps(__location)
+
+    global __location
+    __location = location["data_columns"][3:]
+    return json.dumps(location)
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    with open('artifacts/house_price_prediction.json' , 'rb') as f:
+    global __location
+    getLocation()
+
+    with open('artifacts/house_price_prediction.pickle' , 'rb') as f:
         model= pickle.load(f)
 
-    #to get user values from the html form, use same name as in input tag    
-    x=np.zeros(__location + 3)
-    x[0]=int(request.form['bhk'])
-    x[1]=float(request.form['sqft'])
-    x[2]=int(request.form['bath'])
+    # to get user values from the html form, use same name as in input tag 
+    x=np.zeros(len(__location) + 3)
+    x[0]=request.form['bhk']
+    x[1]=request.form['sqft']
+    x[2]=request.form['bath']
     loc=request.form['location']
 
     try:
@@ -33,11 +38,15 @@ def predict():
     except:
         locIndex=-1
 
-    x[locIndex]=1
+    x[locIndex+3]=1 
 
-    return round(model.predict([x]),2)
+    return str(round(model.predict([x])[0],2))
 
 
+
+@app.route('/')
+def home():
+    return app.send_static_file('app.html')
 
 if __name__ == '__main__':
     app.run()
